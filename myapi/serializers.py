90b6ff300bbd039c1dca2dd85de8d931f4ab2dcc
user <andrushechka.b@gmail.com>
2022-07-21 19:17:47 +0300

    serial number check by mask added

--- myapi/serializers.py
+++ myapi/serializers.py
@@ -12,14 +12,32 @@ class EquipmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Equipment
         fields = ('serial_number', 'note', 'type')
-    def validate_serial_number(self, value):
+    def validate(self, data):
         """
         Check that serial number corresponds to the mask.
         """
-        if len(value) < 3:
-            raise serializers.ValidationError("Length is less than 3 symbols")
-        regex = r'^\d{2}[A-Z]{1}$'
-        ans = re.match(regex, value)
+        serial_number_mask = data['type'].serial_number_mask
+        if len(data['serial_number']) < len(serial_number_mask):
+            raise serializers.ValidationError("Serial number length incorrect")
+
+        #N - digit from 0 to 9
+        #A - capital letter
+        #a - lowercase letter
+        #X - capital letter or digit from 0 to 9
+        #Z - symbol from list: "-", "_", "@"
+        mask_decode = {
+            'N' : r'\d',
+            'A' : r'[A-Z]',
+            'a' : r'[a-z]',
+            'X' : r'[A-Z\d]',
+            'Z' : r'[-_@]',
+        }
+        regex = r'^'
+        for symbol in serial_number_mask:
+            regex += mask_decode[symbol]
+        regex += r'$'
+
+        ans = re.match(regex, data['serial_number'])
         if not ans:
             raise serializers.ValidationError("Doesn't match the mask")
-        return value
+        return data
